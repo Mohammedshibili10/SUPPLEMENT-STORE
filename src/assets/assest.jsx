61cbd5ab41f93156472ec3762b25b735578dab54
Mohammedshibili10 <shibiliek516@gmail.com>
2025-12-31 12:47:55 +0530

    ok

---
+++ src/assets/assest.jsx
@@ -0,0 +1,17 @@
+import back from './images/back.jpg';
+import google from './images/google.png';
+import logo from './images/logo.png';
+import thander from './images/thander.png';
+import rating from './images/rating.png';  
+import protien from './images/protien.jpg';
+import cart from './images/shopping-cart.png';
+import preworkout from './images/preworkout.jpg';
+import omega from './images/omega.jpg';
+
+
+
+
+
+const asset=[back,google,logo,thander,rating,protien,cart,preworkout,omega];
+
+export default asset;
