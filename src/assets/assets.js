94550d68dacbbca5b901297db7517eb436a51750
Mohammedshibili10 <shibiliek516@gmail.com>
2025-12-31 20:33:20 +0530

    updated

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,17 +1,16 @@
 import back from './images/back.jpg';
 import google from './images/google.png';
-import logo from './images/logo.png';
 import thander from './images/thander.png';
 import rating from './images/rating.png';  
 import protien from './images/protien.jpg';
 import cart from './images/shopping-cart.png';
-import preworkout from './images/preworkout.jpg';
+import prework from './images/prework.jpg';
 import omega from './images/omega.jpg';
 
 
 
 
 
-const assets=[back,google,logo,thander,rating,protien,cart,preworkout,omega];
+const assets=[back,google,thander,rating,protien,cart,prework,omega];
 
 export default assets;
