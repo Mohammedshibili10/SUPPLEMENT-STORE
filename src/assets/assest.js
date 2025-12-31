811124e869089c63c1425aee7a3f7104f0a496be
Mohammedshibili10 <shibiliek516@gmail.com>
2025-12-31 12:44:48 +0530

    updated

--- src/assets/assest.js
+++ src/assets/assest.js
@@ -1,12 +1,12 @@
-import back from '../images/back.jpg';
-import google from '../images/google.png';
-import logo from '../images/logo.png';
-import thander from '../images/thander.png';
-import rating from '../images/rating.png';  
-import protien from '../images/protien.jpg';
-import cart from '../images/shopping-cart.png';
-import preworkout from '../images/preworkout.jpg';
-import omega from '../images/omega.jpg';
+import back from './images/back.jpg';
+import google from './images/google.png';
+import logo from './images/logo.png';
+import thander from './images/thander.png';
+import rating from './images/rating.png';  
+import protien from './images/protien.jpg';
+import cart from './images/shopping-cart.png';
+import preworkout from './images/preworkout.jpg';
+import omega from './images/omega.jpg';
 
 
 
